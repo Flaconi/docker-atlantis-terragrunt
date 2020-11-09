70b588a7b1fc6fd5ff43faa07670f09175f56260
runderwoodcr14 <ricardo.underwood@flaconi.de>
2020-11-09 17:12:26 +0100

    OPS-0 Change Ownership of /mnt/efs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/dumb-init /bin/sh
 set -xev
 create_github_user_ssh_key.sh
-mount
 chown atlantis.atlantis /mnt/efs -R
-ls -la /mnt
-whoami
 set -- docker-entrypoint-original.sh "$@"
 exec "$@"
